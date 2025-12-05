fd9bc4ca65f860d48544a19c9622fabe657ed656
root <root@ORDI-Ferdi>
2025-12-05 10:05:46 +0100

    Ajout du RequestMapping et chemin get

--- myfilmlist/src/main/java/com/ensta/myfilmlist/persistence/controller/FilmController.java
+++ myfilmlist/src/main/java/com/ensta/myfilmlist/persistence/controller/FilmController.java
@@ -3,11 +3,13 @@ package com.ensta.myfilmlist.persistence.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ensta.myfilmlist.model.Film;
 import com.ensta.myfilmlist.dto.*;
 import com.ensta.myfilmlist.exception.*;
-public interface FilmController {
 
+public interface FilmController {
+    @GetMapping("/")
     public ResponseEntity<List<FilmDTO>> getAllFilms() throws ControllerException;
 }
--- myfilmlist/src/main/java/com/ensta/myfilmlist/persistence/controller/impl/FilmControllerImpl.java
+++ myfilmlist/src/main/java/com/ensta/myfilmlist/persistence/controller/impl/FilmControllerImpl.java
@@ -6,19 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ensta.myfilmlist.model.Film;
 import com.ensta.myfilmlist.service.MyFilmsService;
 import com.ensta.myfilmlist.dto.*;
 import com.ensta.myfilmlist.exception.*;
 import com.ensta.myfilmlist.mapper.FilmMapper;
-
-@Controller
-public class FilmControllerImpl {
+import com.ensta.myfilmlist.persistence.controller.FilmController;
+@RestController
+@RequestMapping("/film")
+public class FilmControllerImpl implements FilmController {
 
     @Autowired
     private MyFilmsService myFilmsService;
 
+
+    @Override
     public ResponseEntity<List<FilmDTO>> getAllFilms() throws ControllerException {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(FilmMapper.convertFilmToFilmDTOs(myFilmsService.findAll()));
