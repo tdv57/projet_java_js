8635c2071eb702e3fce533649caeb6c41d4544a5
root <root@ORDI-Ferdi>
2025-12-19 10:12:29 +0100

    Changement filmMapper

--- back/src/main/java/com/ensta/myfilmlist/mapper/FilmMapper.java
+++ back/src/main/java/com/ensta/myfilmlist/mapper/FilmMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -18,7 +19,7 @@ import com.ensta.myfilmlist.model.Realisateur;
 /**
  * Effectue les conversions des Films entre les couches de l'application.
  */
-@Repository
+@Component
 public class FilmMapper {
 
 	@Autowired
